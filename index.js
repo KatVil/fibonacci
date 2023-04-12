68159dffe771879ab2e1df5aa322a45d3bad2c5a
Katherine Vilinski-Mazur <Katherine.Vilinski-Mazur@skoltech.ru>
2023-04-12 18:51:47 +0300

    corrected

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 // Итерационный подсчет n-го числа Фибоначчи
 function fibIter(num) {
-    let a = 1;
+    let a = 0;
     let b = 1;
-    for (let i = 3; i <= num; i++) {
-      let c = a + b;
+    let c;
+    for (let i = 0; i <= num; i++) {
+      c = a + b;
       a = b;
       b = c;
     }
+    if (num === 1){
+        return a;
+    } else {
     return b;
+    }
   }
 // Рекурсивное вычисление n-го числа Фибоначчи
 function fibRec (num) {
-    return num <= 1 ? num : fibRec(num - 1) + fibRec(num - 2);
+    if (num === 0){
+        return 1;
+    } else if (num === 1){
+        return 2;
+    }
+    return num <= 0 ? num : fibRec(num - 1) + fibRec(num - 2);
   }
 
 console.log(fibIter(3));
